96ca5353b67a424b6e641526ba737e4dc3a40d62
mananpat <mananpat@yahoo.com>
2026-02-25 18:23:20 -0500

    Duplicate Element Problem

---
+++ CodingProblems/src/main/java/org/codingexercises/problems/DuplicateElement.java
@@ -0,0 +1,43 @@
+package org.codingexercises.problems;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given set of array, check if array contains duplicate element.
+ *
+ * Time Complexity:
+ *        it iterates over an array O(N),
+ *        HashSet operation is O(1)
+ *        Overall time complexity: O(N)
+ *  Space Complexity:
+ *       HashSet uses N space ( worse case to store all elements).
+ *        Overall Space Complexity: O(N)
+ */
+public class DuplicateElement {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        int[] input1 = new int[]{1,2,3,4};
+        DuplicateElement de = new DuplicateElement();
+        boolean result1 = de.containsDuplicate(input1);
+        System.out.printf("Result 1: [%s]%n", result1);
+
+        int[] input2 = new int[]{3,2,3,4};
+        boolean result2 = de.containsDuplicate(input2);
+        System.out.printf("Result 2: [%s]%n", result2);
+    }
+
+    public boolean containsDuplicate(int[] nums){
+        Set<Integer> counterSet = new HashSet<>();
+        for(int num : nums){
+            if(!counterSet.add(num))
+                return true;
+        }
+        return false;
+    }
+}
