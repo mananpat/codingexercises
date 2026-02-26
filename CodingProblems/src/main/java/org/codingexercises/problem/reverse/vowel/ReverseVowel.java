f052625ee1e488afd4d2ef3573e2d95db4e863dd
mananpat <mananpat@yahoo.com>
2026-02-25 20:55:58 -0500

    reverse vowel solution

---
+++ CodingProblems/src/main/java/org/codingexercises/problem/reverse/vowel/ReverseVowel.java
@@ -0,0 +1,43 @@
+package org.codingexercises.problem.reverse.vowel;
+
+/*
+Giving input string, reverse vowel characters.
+
+Two Pointers traversal. first and last pointers.
+Linear scan of the characters.
+Vowel Check: O(1)
+ */
+public class ReverseVowel {
+
+
+    public static void main(String[] args){
+        ReverseVowel solution = new ReverseVowel();
+        String input = "abbded";
+
+        String output = solution.reverseString(input);
+        System.out.println("output String: " + output);
+
+    }
+
+    public String reverseString(String input){
+        String vowel = "aeiouAEIOU";
+        int first = 0;
+        int last = input.length()-1;
+        char[] array = input.toCharArray();
+        while(first < last){
+            while (first < last && vowel.indexOf(array[first]) == -1)
+                first++;
+            while(first < last && vowel.indexOf(array[last]) == -1)
+                last--;
+
+            char temp = array[first];
+            array[first] = array[last];
+            array[last] = temp;
+            first++;
+            last--;
+        }
+
+        return new String(array);
+
+    }
+}
